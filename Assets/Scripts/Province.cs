4c0de6274ddf026fad5e5638f5577d6742995b9f
David Horler <davidhorler@outlook.com>
2017-07-19 21:40:57 +0100

    added groundwork. Character, Culture, Province, Title, and also created a test scene with the NYC boroughs

---
+++ Assets/Scripts/Province.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Province : MonoBehaviour {
+    ///<summary>The "county" Title associated with this province.</summary>
+    public Title title;
+
+    ///<summary>The predominant Culture of the province.</summary>
+    public Culture culture;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
